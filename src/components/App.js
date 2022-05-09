621f085c61f3f498ee67a04aa4c30edeaa8837e1
AniketDutta007 <aniket2018dutta@gmail.com>
2022-05-10 03:43:07 +0530

    added home page, navbar and loader

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,35 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+
 import { getPosts } from '../api';
+import { Home } from '../pages';
+import { Loader, Navbar } from './';
 
 function App() {
-  useEffect(async () => {
-    const response = await getPosts();
-    console.log('response', response);
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await getPosts();
+
+      if (response.success) {
+        setPosts(response.data.posts);
+      }
+
+      setLoading(false);
+    };
+
+    fetchPosts();
   }, []);
+
+  if (loading) {
+    return <Loader />;
+  }
+
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <Navbar />
+      <Home posts={posts} />
     </div>
   );
 }

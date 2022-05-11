08706da3c6547851b75765c79c58cc1eb4cdce52
AniketDutta007 <aniket2018dutta@gmail.com>
2022-05-11 15:00:31 +0530

    added feature to keep user logged in after refresh using local storage

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,28 @@
 export * from './constants';
 
+export const setItemInLocalStorage = (key, value) => {
+  if (!key || !value) {
+    return console.error('Can not store in local storage');
+  }
+  const valueToStore =
+    typeof value !== 'string' ? JSON.stringify(value) : value;
+  localStorage.setItem(key, valueToStore);
+};
+
+export const getItemFromLocalStorage = (key) => {
+  if (!key) {
+    return console.error('Can not get item from local storage');
+  }
+  return localStorage.getItem(key);
+};
+
+export const removeItemFromLocalStorage = (key) => {
+  if (!key) {
+    return console.error('Can not reomve item from local storage');
+  }
+  return localStorage.removeItem(key);
+};
+
 export const getFormBody = (params) => {
   let formBody = [];
   for (let property in params) {
